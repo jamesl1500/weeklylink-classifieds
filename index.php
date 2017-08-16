0284cb738aeca10d47e09f0c839dcadb0b7f42fd
James Latten JR <james@sitelyftstudios.com>
2017-08-16 11:02:58 -0400

    Initial

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <title>Weekly Link Checkout</title>
+
+    <!-- META SEO -->
+    <meta charset="UTF-8">
+    <meta name="description" content="Description goes here">
+    <meta name="keywords" content="keywords go here">
+    <meta name="author" content="Author goes here">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- STYLESHEETS -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Titillium+Web:300,400,600,700">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css" />
+
+    <script src="https://use.fontawesome.com/780689fece.js"></script>
+</head>
+<body>
+
+<!-- SCRIPTS -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="js/site.js"></script>
+</body>
+</html>
